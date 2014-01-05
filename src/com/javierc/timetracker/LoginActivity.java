b3c53d57d303508eefd4f04caa7e49735e68b8e2
Javier C <javier@javierachavez.com>
2014-01-05 15:26:19 -0700

    incorrect uname pwd info added to login

--- src/com/javierc/timetracker/LoginActivity.java
+++ src/com/javierc/timetracker/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.javierc.timetracker;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import com.javierc.timetracker.com.javierc.api.NewLoginTask;
 
 import java.util.concurrent.ExecutionException;
@@ -41,6 +44,9 @@ public class LoginActivity extends Activity {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(getWindow().getCurrentFocus().getWindowToken(), 0);
+
                 pref = getSharedPreferences("lgen", MODE_PRIVATE);
                 SharedPreferences.Editor pedit = pref.edit();
                 String pwd = editTextPassword.getText().toString();
@@ -62,7 +68,8 @@ public class LoginActivity extends Activity {
                     finish();
                 }
                 else {
-
+                    TextView tv = (TextView)findViewById(R.id.respMsg);
+                    tv.setText("Check Username or Password");
                 }
             }
         };
