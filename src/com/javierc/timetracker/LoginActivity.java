9879d2c6ec1dddb488b84f3e80c27f267c425b74
Javier C <javier@javierachavez.com>
2014-01-05 00:59:37 -0700

    creating seperate classes to handle API, designing login

--- src/com/javierc/timetracker/LoginActivity.java
+++ src/com/javierc/timetracker/LoginActivity.java
@@ -4,9 +4,20 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.util.concurrent.ExecutionException;
+
 
 /**
  * Created by Javier
@@ -45,16 +56,52 @@ public class LoginActivity extends Activity {
                 pedit.putString("username", usr);
                 pedit.putString("password", pwd);
                 pedit.commit();
-                finish();
+                String[] params = new String[]{usr,pwd};
+                String res = "";
+                try {
+                     res = new CheckCredsTask().execute(params).get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+                if(!res.isEmpty()) { finish(); }
             }
         };
     }
 
     private class CheckCredsTask extends AsyncTask<String,Object,String>{
 
+
         @Override
         protected String doInBackground(String[] strings) {
-            return null;
+            String returned = "";
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            try {
+                httpclient.getCredentialsProvider().setCredentials(
+                        new AuthScope(AuthScope.ANY_HOST, 443),
+                        new UsernamePasswordCredentials(strings[0], strings[1]));
+
+                HttpGet httpget = new HttpGet("ttcheckin.eu01.aws.af.cm");
+
+                Log.d("req ", String.valueOf(httpget.getRequestLine()));
+                HttpResponse response = httpclient.execute(httpget);
+                HttpEntity entity = response.getEntity();
+
+                Log.d("status ", String.valueOf(response.getStatusLine()));
+                if (entity != null) {
+                    Log.d("len ", String.valueOf(entity.getContentLength()));
+                    System.out.println(EntityUtils.toString(entity));
+                    returned = EntityUtils.toString(entity);
+                }
+            } catch(Exception e){
+                e.printStackTrace();
+            }finally {
+                httpclient.getConnectionManager().shutdown();
+            }
+
+            return returned;
         }
+
     }
 }
---
+++ src/com/javierc/timetracker/com/javierc/api/CheckinStatus.java
@@ -0,0 +1,7 @@
+package com.javierc.timetracker.com.javierc.api;
+
+/**
+ * Created by javierAle on 1/5/14.
+ */
+public class CheckinStatus {
+}
