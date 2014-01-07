afaff3251a357e4e90447085ebe4431dea78e010
Javier C <javier@javierachavez.com>
2014-01-07 12:54:14 -0700

    add date time to checkin

--- src/com/javierc/timetracker/API/UpdateCheckIn.java
+++ src/com/javierc/timetracker/API/UpdateCheckIn.java
@@ -1,13 +1,16 @@
 package com.javierc.timetracker.API;
 
 import android.content.Context;
-//import android.util.Log;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
@@ -15,6 +18,8 @@ import org.json.JSONObject;
  * Created by javierAle on 1/5/14.
  */
 public class UpdateCheckIn extends Updater<String,Object,String> {
+    public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ssXXX";
+
 
     public UpdateCheckIn(Context context){
         this.context = context;
@@ -29,8 +34,18 @@ public class UpdateCheckIn extends Updater<String,Object,String> {
         try {
             defaultHttpClient.getCredentialsProvider().setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT),
                     new UsernamePasswordCredentials(this.getU(), this.getP()));
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+
+            JSONObject jsonObj = new JSONObject();
+            jsonObj.put("time", sdf.format(cal.getTime()));
+
+            StringEntity postEntity = new StringEntity(jsonObj.toString(), HTTP.UTF_8);
+            postEntity.setContentType("application/json");
 
             HttpPost httpPost = new HttpPost(API.CHECKIN_URL.string());
+
+            httpPost.setEntity(postEntity);
 
             // Log.d("req ", String.valueOf(httpPost.getRequestLine()));
             HttpResponse response = defaultHttpClient.execute(httpPost);
