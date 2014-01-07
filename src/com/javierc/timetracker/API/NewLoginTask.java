c73f7e55774cd878790a262488972862fa045ec0
Javier C <javier@javierachavez.com>
2014-01-06 20:08:08 -0700

    removed unused code

--- src/com/javierc/timetracker/API/NewLoginTask.java
+++ src/com/javierc/timetracker/API/NewLoginTask.java
@@ -1,11 +1,8 @@
 package com.javierc.timetracker.API;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.view.View;
-import android.widget.ProgressBar;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
