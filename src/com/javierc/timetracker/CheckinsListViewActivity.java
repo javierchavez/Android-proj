467eb13e2e17d2dde3056575ac8580e57e89e89d
Javier C <javier@javierachavez.com>
2014-01-12 23:42:53 -0700

    implementing list checkins

--- src/com/javierc/timetracker/CheckinsListViewActivity.java
+++ src/com/javierc/timetracker/CheckinsListViewActivity.java
@@ -1,10 +1,11 @@
 package com.javierc.timetracker;
 
+import android.app.Activity;
 import android.app.ListActivity;
 
 /**
  * Created by javierAle on 1/4/14.
  */
-public class CheckinsListViewActivity extends ListActivity {
+public class CheckinsListViewActivity extends Activity {
 
 }
